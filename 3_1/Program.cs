b2a17f180c528577152ebcbd09b90a3375c5d5fe
Admin <Admin@TEB123007>
2023-10-20 15:05:30 +0200

    Add project files.

---
+++ 3_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Vänligen ange din ålder så vi vet om du kan delta");
+        if (int.TryParse(Console.ReadLine(), out int age))
+        {
+            if (age >= 19)
+            {
+                Console.WriteLine("Du är för gammal för att vara med.");
+            }
+            else if (age < 16)
+            {
+                Console.WriteLine("Du är för ung för att delta i denna tävlingen.");
+            }
+            else
+            {
+                Console.WriteLine("Du får vara med i tävlingen!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("det är en ogiltig ålder. Var god ange en giltig ålder.");
+        }
+    }
+}
